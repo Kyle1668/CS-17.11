2db544f28c780c8b6c12b9f0ea7bf5520052a2af
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-03 16:12:10 -0700

    cc

--- Assignments/A6_OBrien_Kyle/src/edu/srjc/A6/calculator/Fraction.java
+++ Assignments/A6_OBrien_Kyle/src/edu/srjc/A6/calculator/Fraction.java
@@ -1,8 +1,39 @@
 package edu.srjc.A6.calculator;
 
-/**
- * Created by kyleobrien on 11/2/17.
- */
+
 public class Fraction
 {
+    private String numerator = "";
+    private String denominator = "";
+
+    public Fraction(String numerator, String denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    void add(Fraction otherFraction)
+    {
+
+    }
+
+    void subtract(Fraction otherFraction)
+    {
+
+    }
+
+    void multiply(Fraction otherFraction)
+    {
+
+    }
+
+    void divide(Fraction otherFraction)
+    {
+    }
+
+    private String value()
+    {
+       return numerator + "/" + denominator;
+    }
+
 }
