8760ae12889011d0035ee0c00e2e4de240f51dc9
Kyle1668 <kyledevinobrien1@gmail.com>
2017-11-16 23:18:43 -0800

    gg

--- Assignments/A7_OBrien_Kyle_EnergyUsageUI/src/edu/srjc/A7/EnergyUsageUI/FXMLDocumentController.java
+++ Assignments/A7_OBrien_Kyle_EnergyUsageUI/src/edu/srjc/A7/EnergyUsageUI/FXMLDocumentController.java
@@ -1,5 +1,9 @@
 package edu.srjc.A7.EnergyUsageUI;
 
+import edu.srjc.A7.EnergyUsageUI.Models.*;
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -36,7 +40,7 @@ public class FXMLDocumentController implements Initializable
     @FXML
     private MenuItem mnuFileExit;
 
-    @FXML private LineChart<String, Number> bpChart;
+    @FXML static private LineChart<String, Number> dataVisualizationChart;
     private CategoryAxis dateAxis = new CategoryAxis();
     private NumberAxis valueAxis = new NumberAxis();
 
@@ -44,8 +48,166 @@ public class FXMLDocumentController implements Initializable
     public static final String gasFile = "pge_gas_interval_data_2016-01-01_to_2016-03-01.csv";
     public static final String electricFile = "pge_electric_interval_data_2016-01-01_to_2016-02-28.csv";
 
+    private static void initVisualization()
+    {
+        ArrayList<GasDataPoint> dailyGasUsage = CSVParseOperations.parseGasData(gasFile);
+        ArrayList<ElectricDataPoint> dailyPowerUsage = CSVParseOperations.parseElectricData(electricFile);
+        ArrayList<TemperatureDataPoint> dailyTemperatureData = CSVParseOperations.parseTemperatureData(temperatureFile);
+        ArrayList<HomeDataPoint> dailyHomeData = getHomeData(dailyGasUsage, dailyPowerUsage, dailyTemperatureData);
 
-    class DataPopup extends StackPane
+        XYChart.Series electricUsageSeries = new XYChart.Series();
+        XYChart.Series naturalGasUsageSeries = new XYChart.Series();
+        XYChart.Series lowestTemperatureSeries = new XYChart.Series();
+        XYChart.Series highestTemperatureSeries = new XYChart.Series();
+
+        electricUsageSeries.setName("Electric");
+        naturalGasUsageSeries.setName("Gas");
+        lowestTemperatureSeries.setName("Lowest Temperature");
+        highestTemperatureSeries.setName("Highest Temperature");
+
+        for (HomeDataPoint homeData : dailyHomeData)
+        {
+
+            String gasLabel = "gasLabel";
+            String electricLabel = "electricLabel";
+            String lowestTempLabel = "lowestTempLabel";
+            String highestTempLabel = "highestTempLabel";
+
+//            String labelData = String.format("Date: %s\nSyst: %s\nDias: %s\nPulse: %s\n",
+//                    r.getDate(), r.getSystolic(), r.getDiastolic(), r.getHeartRate());
+
+            String currentDate = homeData.getDate();
+
+            XYChart.Data<String, Double> gasVisualizationDataPoint = new XYChart.Data<>(currentDate, homeData.getCumulativeGasUsage());
+            XYChart.Data<String, Double> electricVisualizationDataPoint = new XYChart.Data<>(currentDate, homeData.getCumulativeElectricUsage());
+            XYChart.Data<String, Float> lowestTempVisualizationDataPoint = new XYChart.Data<>(currentDate, homeData.getLowestTemperature());
+            XYChart.Data<String, Float> highestTempVisualizationDataPoint = new XYChart.Data<>(currentDate, homeData.getHighestTemperature());
+
+            naturalGasUsageSeries.setNode(new DataPopup(gasLabel));
+            electricUsageSeries.setNode(new DataPopup(electricLabel));
+            lowestTemperatureSeries.setNode(new DataPopup(lowestTempLabel));
+            highestTemperatureSeries.setNode(new DataPopup(highestTempLabel));
+
+            naturalGasUsageSeries.getData().add(gasVisualizationDataPoint);
+            electricUsageSeries.getData().add(electricVisualizationDataPoint);
+            lowestTemperatureSeries.getData().add(lowestTempVisualizationDataPoint);
+            highestTemperatureSeries.getData().add(highestTempVisualizationDataPoint);
+
+        }
+
+        dataVisualizationChart.getData().add(naturalGasUsageSeries);
+        dataVisualizationChart.getData().add(electricUsageSeries);
+        dataVisualizationChart.getData().add(lowestTemperatureSeries);
+        dataVisualizationChart.getData().add(highestTemperatureSeries);
+
+    }
+
+    private static boolean sameDate(String date1, String date2, String date3)
+    {
+        return date1.equals(date2) && date2.equals(date3);
+    }
+
+    private static String getCommonStartDate(String date1, String date2, String date3)
+    {
+        String latestDate = "";
+
+        try {
+            // Converts String date to epoc.
+            Long epocDate1 = new SimpleDateFormat("yyyy-MM-dd").parse(date1).getTime();
+            Long epocDate2 = new SimpleDateFormat("yyyy-MM-dd").parse(date2).getTime();
+            Long epocDate3 = new SimpleDateFormat("yyyy-MM-dd").parse(date3).getTime();
+
+            // Finds largest epoc time.
+            Long largestEpocTime = Math.max(epocDate1, Math.max(epocDate2, epocDate3));
+
+            latestDate = new SimpleDateFormat("yyyy-MM-dd").format(largestEpocTime);
+            System.out.print("latestDate: " + latestDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return latestDate;
+    }
+
+    private static ArrayList<HomeDataPoint> getHomeData(ArrayList<GasDataPoint> dailyGasUsage,
+                                                        ArrayList<ElectricDataPoint> dailyElectricUsage,
+                                                        ArrayList<TemperatureDataPoint> dailyTemperatureData)
+    {
+        ArrayList<HomeDataPoint> dailyHomeData = new ArrayList<>();
+
+        int upperBound = Math.min(dailyGasUsage.size(), Math.min(dailyElectricUsage.size(), dailyTemperatureData.size()));
+        int index = 0;
+
+        String gasDate = dailyGasUsage.get(0).getDate();
+        String electricDate = dailyElectricUsage.get(0).getDate();
+        String temperatureDate = dailyTemperatureData.get(0).getDate();
+        String startDate = getCommonStartDate(gasDate, electricDate, temperatureDate);
+
+        for (int i = 0; i < dailyGasUsage.size(); i++)
+        {
+            if (!dailyGasUsage.get(i).getDate().equals(startDate))
+            {
+                dailyGasUsage.remove(i);
+                i--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        for (int i = 0; i < dailyElectricUsage.size(); i++)
+        {
+            if (!dailyElectricUsage.get(i).getDate().equals(startDate))
+            {
+                dailyElectricUsage.remove(i);
+                i--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        for (int i = 0; i < dailyTemperatureData.size(); i++)
+        {
+            if (!dailyTemperatureData.get(i).getDate().equals(startDate))
+            {
+                dailyTemperatureData.remove(i);
+                i--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        while (index < upperBound)
+        {
+            gasDate = dailyGasUsage.get(index).getDate();
+            electricDate = dailyElectricUsage.get(index).getDate();
+            temperatureDate = dailyTemperatureData.get(index).getDate();
+
+            if (sameDate(gasDate, electricDate, temperatureDate))
+            {
+                float highestTemp = dailyTemperatureData.get(index).getHighestTemperature();
+                float lowestTemp = dailyTemperatureData.get(index).getLowestTemperature();
+                double electricUsage = dailyElectricUsage.get(index).getCumulativeUsage();
+                double gasUsage = dailyGasUsage.get(index).getUsage();
+
+                dailyHomeData.add(new HomeDataPoint(gasDate, lowestTemp, highestTemp, gasUsage, electricUsage));
+            }
+
+            index++;
+        }
+
+        return dailyHomeData;
+    }
+
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    static class DataPopup extends StackPane
     {
         public DataPopup(String content)
         {
---
+++ Assignments/A7_OBrien_Kyle_EnergyUsageUI/src/edu/srjc/A7/EnergyUsageUI/Models/CSVParseOperations.java
@@ -0,0 +1,248 @@
+package edu.srjc.A7.EnergyUsageUI.Models;
+
+import edu.srjc.A7.EnergyUsageUI.Models.GasDataPoint;
+import edu.srjc.A7.EnergyUsageUI.Models.ElectricDataPoint;
+import edu.srjc.A7.EnergyUsageUI.Models.TemperatureDataPoint;
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
+public class CSVParseOperations
+{
+    static public ArrayList<ElectricDataPoint> parseElectricData(String fileName)
+    {
+        ArrayList<ElectricDataPoint> dailyPowerUsage = new ArrayList<ElectricDataPoint>();
+        File powerUsageFile = new File(System.getProperty("user.dir") + "/" + fileName);
+
+        if (powerUsageFile.exists())
+        {
+            Scanner csvReader = null;
+
+            try
+            {
+                csvReader = new Scanner(powerUsageFile);
+                dailyPowerUsage = getElectricUsageSummary(csvReader);
+//                printElectricUsageSummary(dailyPowerUsage);
+                csvReader.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.println("Cannot open that file.");
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
+
+        return dailyPowerUsage;
+    }
+
+    private static ArrayList<ElectricDataPoint> getElectricUsageSummary(Scanner fReader)
+    {
+        ArrayList<ElectricDataPoint> dailyPowerUsage = new ArrayList<>();
+        double lineUsageData = 0.0;
+        String lineDate = "N/A";
+
+        dailyPowerUsage.add(new ElectricDataPoint());
+
+        while (fReader.hasNextLine())
+        {
+            // Receives a line from the CSV file, parses it and then assigns it to inputDataLine.
+
+            ElectricDataPoint lastDateRead = dailyPowerUsage.get(dailyPowerUsage.size() - 1);
+            String[] inputDataLine = fReader.nextLine().split(",");
+
+            if (inputDataLine[0].equals("Electric usage"))
+            {
+                lineUsageData = Double.parseDouble(inputDataLine[4]);
+                lineDate = inputDataLine[1];
+
+                if (lineDate.equals(lastDateRead.getDate()))
+                {
+                    // Increment the most recent date's cumulative usage by the current line's (hour) usage.
+                    double newValue = dailyPowerUsage.get(dailyPowerUsage.size() - 1).getCumulativeUsage() + lineUsageData;
+                    dailyPowerUsage.get(dailyPowerUsage.size() - 1).setCumulativeUsage(newValue);
+                }
+                else
+                {
+                    // Creates new date object. Sets that new date's data to that of the current line.
+                    // The new date is then appended to the ArrayList dailyPowerUsage.
+
+                    ElectricDataPoint newDate = new ElectricDataPoint(lineDate, lineUsageData);
+                    dailyPowerUsage.add(newDate);
+                }
+            }
+
+        }
+
+        dailyPowerUsage.remove(0);
+
+        return dailyPowerUsage;
+    }
+
+    public static void printElectricUsageSummary(ArrayList<ElectricDataPoint> dailyPowerUsage)
+    {
+        for (ElectricDataPoint day : dailyPowerUsage)
+        {
+            if (!day.getDate().equals("N/A"))
+            {
+                System.out.println(day.toString());
+            }
+        }
+    }
+
+    static public ArrayList<GasDataPoint> parseGasData(String fileName)
+    {
+        ArrayList<GasDataPoint> data = new ArrayList<>();
+        File gasUsageFile = new File(System.getProperty("user.dir") + "/" + fileName);
+
+        if (gasUsageFile.exists())
+        {
+            Scanner csvReader = null;
+
+            try
+            {
+                csvReader = new Scanner(gasUsageFile);
+                data = getGasUsageSummary(csvReader);
+//                printGasUsageSummary(data);
+                csvReader.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.println("Cannot open that file.");
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
+
+        return data;
+    }
+
+    private static ArrayList<GasDataPoint> getGasUsageSummary(Scanner fReader)
+    {
+        ArrayList<GasDataPoint> data = new ArrayList<>();
+        double lineUsageData = 0.0;
+        String lineDate = "N/A";
+
+        while (fReader.hasNextLine())
+        {
+            String[] inputDataLine = fReader.nextLine().split(",");
+
+            if (inputDataLine[0].equals("Natural gas usage"))
+            {
+                lineDate = inputDataLine[1];
+                lineUsageData = Double.parseDouble(inputDataLine[2]);
+                data.add(new GasDataPoint(lineDate, lineUsageData));
+            }
+
+        }
+
+        return data;
+    }
+
+    public static void printGasUsageSummary(ArrayList<GasDataPoint> dailyGasUsage)
+    {
+        for (GasDataPoint day : dailyGasUsage)
+        {
+            if (!day.getDate().equals("N/A"))
+            {
+                System.out.println(day.toString());
+            }
+        }
+    }
+
+    static public ArrayList<TemperatureDataPoint> parseTemperatureData(String fileName)
+    {
+        Scanner csvReader = null;
+        File csvWeatherDataFile = new File(System.getProperty("user.dir") + "/" + fileName);
+
+        ArrayList<TemperatureDataPoint> allWeatherData = new ArrayList<>();
+
+        if (csvWeatherDataFile.exists())
+        {
+            try
+            {
+                csvReader = new Scanner(csvWeatherDataFile);
+                String output = "";
+
+                while (csvReader.hasNextLine())
+                {
+                    output = csvReader.nextLine();
+
+                    if (!output.equals("") && output.charAt(0) != '#' && output.split(",").length >= 7)
+                    {
+
+                        Float outsideTemperature = Float.parseFloat(formatData(output.split(",")[7]));
+
+                        if (allWeatherData.size() != 0)
+                        {
+                            TemperatureDataPoint newData = new TemperatureDataPoint(output);
+                            TemperatureDataPoint lastData = allWeatherData.get(allWeatherData.size() - 1);
+
+                            String lastDay = lastData.getDate().split(" ")[0];
+                            String currentDay = newData.getDate().split(" ")[0];
+
+                            if (lastDay.equals(currentDay))
+                            {
+                                if (lastData.getLowestTemperature() != 0.0 && lastData.getHighestTemperature() != 0.0)
+                                {
+                                    if (outsideTemperature < lastData.getLowestTemperature())
+                                    {
+                                        lastData.setLowestTemperature(outsideTemperature);
+                                    }
+                                    else if (outsideTemperature > lastData.getHighestTemperature())
+                                    {
+                                        lastData.setHighestTemperature(outsideTemperature);
+                                    }
+                                }
+                                else
+                                {
+                                    lastData.setHighestTemperature(outsideTemperature);
+                                    lastData.setLowestTemperature(outsideTemperature);
+                                }
+                            }
+                            else
+                            {
+                                allWeatherData.add(newData);
+                            }
+                        }
+                        else
+                        {
+                            TemperatureDataPoint newDataPoint = new TemperatureDataPoint(output);
+                            allWeatherData.add(newDataPoint);
+                        }
+
+                    }
+
+                }
+
+                csvReader.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.println("Cannot open that file.");
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
+        else
+        {
+            System.out.println("That file doesn't exist. Try again");
+        }
+
+        return allWeatherData;
+    }
+
+    public static void printTemperatureSummary(ArrayList<TemperatureDataPoint> dailyPowerUsage)
+    {
+        for (TemperatureDataPoint day : dailyPowerUsage)
+        {
+            day.printData();
+        }
+    }
+
+    public static String formatData(String arg)
+    {
+        // Removes extra quotations from csv data to avoid type casting errors.
+        return !arg.equals("") ? arg.substring(1, arg.length() - 1) : arg;
+    }
+
+}
