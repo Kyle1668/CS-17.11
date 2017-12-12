8dcd12241027f0d71f4aa6beb163da2f9b4b7356
Kyle1668 <kyledevinobrien1@gmail.com>
2017-12-11 21:33:16 -0800

    Hello World!

--- Final/Final_OBrien_Kyle_QuickCafe/SaveRepo.py
+++ Final/Final_OBrien_Kyle_QuickCafe/SaveRepo.py
@@ -1,6 +1,6 @@
 import os
 
-message = raw_input("Enter Commit Message: ")
+message = raw_input("\n" + "Enter Commit Message: ")
 commit_command = "git commit -m " + "\"" + str(message) + "\""
 
 os.system("git status")
@@ -8,4 +8,4 @@ os.system("git add .")
 os.system(commit_command)
 os.system("git push -u origin master")
 
-print("Script Completed")
+print("Script Completed\n")
