cf464679357894e5b1dbe2cbefcce49978e5df3c
Kyle1668 <kyledevinobrien1@gmail.com>
2017-12-11 21:32:31 -0800

    Added progress

--- Final/Final_OBrien_Kyle_QuickCafe/SaveRepo.sh
+++
@@ -1,7 +0,0 @@
-# I've been using a private Github repository to save my work and revert to previous commits when I've broken my code.
-# This script runs the commands necessary to save my changes, make a commit, and push the changes to the remote repo.
-
-git status
-git add .
-git commit -m "save progress"
-git push
